8df76e3e96d13077cf95db7c8d30c82281970c1d
dev-pc <dev-pc@DESKTOP-82HDSM9>
2024-10-16 02:58:48 +0200

    kestrel added

--- USP.API/Program.cs
+++ USP.API/Program.cs
@@ -7,6 +7,17 @@ using USP.Worker;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Kestrel configuration (if you want to customize the ports or other options)
+builder.WebHost.UseKestrel(options =>
+{
+    options.ListenAnyIP(5041);  // HTTP
+    options.ListenAnyIP(5042, listenOptions =>
+    {
+        listenOptions.UseHttps();  // HTTPS
+    });
+});
+
+// Add services and middlewares...
 builder.Services.AddControllers(options => options.Filters.Add<ApiExceptionFilter>());
 builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddFluentValidationClientsideAdapters();
@@ -21,7 +32,22 @@ builder.Services.AddSingleton<IProductService, ProductService>();
 builder.Services.AddHostedService<NotifyUserWorker>();
 builder.Services.AddHostedService<UpdateProductEmbeddedWorker>();
 
+
+
+builder.Services.AddCors(options =>
+{
+     options.AddPolicy("AllowAll",
+         policy =>
+         {
+             policy.AllowAnyOrigin()
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+         });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAll"); // Use the policy defined above
 
 if (app.Environment.IsDevelopment())
 {
@@ -29,9 +55,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.MapControllers();
-
 app.Run();
+
 
 public partial class Program { }
